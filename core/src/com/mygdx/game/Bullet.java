bad9d69815cf39fcd16e33dbc3392323422d2890
ElenaR <llinusik@mail.ru>
2023-11-20 17:28:08 +0300

    ver 1.2

--- core/src/com/mygdx/game/Bullet.java
+++ core/src/com/mygdx/game/Bullet.java
@@ -2,45 +2,38 @@ package com.mygdx.game;
 
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Vector2;
 
 public class Bullet {
     private Texture texture;
 
-    private float x;
-    private float y;
-    private float vx; //скорость по х
-    private float vy; // скорость по у
+    private Vector2 position;
+    private Vector2 velocity; // скорость (без  ускорения)
     private float speed;
     private boolean active; // может быть включена или выключена
 
-    public Bullet() {
-        this.texture = new Texture("projectile.png");
-    }
+
 
     public boolean isActive() {
         return active;
     }
 
-    public Bullet(float x, float y, float speed, boolean active) {
+    public Bullet() {
         this.texture = new Texture("projectile.png");
-        this.x = 0.0f;
-        this.y = 0.0f;
-        this.vx = 0.0f;
-        this.vy = 0.0f;
+        this.position = new Vector2();
+        this.velocity = new Vector2();
         this.active = false;
     }
 
     public void render (SpriteBatch batch){
-        batch.draw (texture, x -8, y - 8); //вычитаем половину из картинки для централизации
+        batch.draw (texture, position.x -8, position.y - 8); //вычитаем половину из картинки для централизации
     }
 
 
     public void active (float x, float y, float vx, float vy){
         this.active = true;
-        this.x = x;
-        this.y = y;
-        this.vx = vx;
-        this.vy = vy;
+        this.position.set(x, y);
+        this.velocity.set(vx, vy);
     }
 
     public void deactivate(){
@@ -48,10 +41,8 @@ public class Bullet {
     }
 
     public void update (float dt){
-        x += vx * dt;
-        y += vy * dt;
-
-        if (x < 0.0f || x > 1280f || y < 0.0f || y > 720.f) {
+       position.mulAdd(velocity, dt); //складываем два вектора
+        if (position.x < 0.0f || position.x > 1280f || position.y < 0.0f || position.y > 720.f) {
             deactivate();
         }
 
--- core/src/com/mygdx/game/Tank.java
+++ core/src/com/mygdx/game/Tank.java
@@ -4,27 +4,26 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Vector2;
 
 public class Tank {
     private MyGdxGameTank game;
     private Texture texture;
-    private float x;
-    private float y;
+    private Vector2 position;
     private float speed;
     private float angle; //есть у танка угол
 
     public Tank (MyGdxGameTank game){
         this.game = game;
-        this.texture = new Texture("tank.png");
-        this.x = 100.0f;
-        this.y = 100.0f;
+        this.texture = new Texture("player_tank_base.png");
+        this.position = new Vector2(100.f, 100.0f);
         this.speed = 100.0f;
 
 
     }
     public void render (SpriteBatch batch){ //рисуем танк
         //ниже описываем повороты танка
-        batch.draw(texture,x-16,y-16,16,16,32, 32,1,1,angle,0, 0, 32,32, false,false);
+        batch.draw(texture,position.x-20,position.y-20,20,20,40, 40,1,1,angle,0, 0, 40,40, false,false);
     }
 
     public void update (float dt){
@@ -36,22 +35,22 @@ public class Tank {
 
     public void checkMoment(float dt){ //метод отвечает за движения
         if (Gdx.input.isKeyPressed(Input.Keys.LEFT)){
-            x -= speed * dt;
+            position.x -= speed * dt;
             angle = 180.f;
             return;
         }
         if (Gdx.input.isKeyPressed(Input.Keys.RIGHT)){
-            x += speed * dt;
+            position.x += speed * dt;
             angle = 0;
             return;
         }
         if (Gdx.input.isKeyPressed(Input.Keys.UP)){
-            y += speed * dt;
+            position.y += speed * dt;
             angle = 90.0f;
             return;
         }
         if (Gdx.input.isKeyPressed(Input.Keys.DOWN)){
-            y -= speed * dt;
+            position.y -= speed * dt;
             angle = 270.0f;
             return;
         }
@@ -60,7 +59,7 @@ public class Tank {
     public void fire () {
         if (!game.getBullet().isActive()) { //проверяем не активна пуля
             float angleRad = (float) Math.toRadians(angle); //приводим к радиану
-            game.getBullet().active(x, y, 320.0f * (float) Math.cos(angleRad),320.0f * (float) Math.sin(angleRad)); // хотим активировать и указываем координаты
+            game.getBullet().active(position.x, position.y, 320.0f * (float) Math.cos(angleRad),320.0f * (float) Math.sin(angleRad)); // хотим активировать и указываем координаты
         }
     }
 }
